1b339439294b8f02813d9ab1b5415ad760730b06
Gabriel <12038578+gabrielgrover@users.noreply.github.com>
2022-11-01 17:44:20 -0700

    cache theme

--- Providers/ThemeProvider.tsx
+++ Providers/ThemeProvider.tsx
@@ -6,26 +6,42 @@ type ThemeProviderContextType = {
   toggle_theme: () => void;
 };
 
+const VOID_LOG_THEME = "VOID_LOG_THEME";
+
 export const ThemeContext = React.createContext<ThemeProviderContextType>({
   theme: "light",
   toggle_theme: () => {},
 });
 
 export const ThemeProvider: React.FC<React.PropsWithChildren> = (props) => {
-  const [theme, set_theme] = React.useState<"light" | "dark">("light");
+  const [theme, set_theme] = React.useState<"light" | "dark">();
 
   const toggle_theme = () =>
     set_theme((prev_them) => {
       if (prev_them === "light") {
+        window.localStorage.setItem(VOID_LOG_THEME, "dark");
         return "dark";
       }
 
+      window.localStorage.setItem(VOID_LOG_THEME, "light");
       return "light";
     });
 
   React.useEffect(() => {
     document.body.dataset.theme = theme;
   }, [theme]);
+
+  React.useEffect(() => {
+    const cached_theme = window.localStorage.getItem(VOID_LOG_THEME);
+
+    if (cached_theme) {
+      set_theme(cached_theme as "light" | "dark");
+    }
+  }, []);
+
+  if (!theme) {
+    return null;
+  }
 
   return (
     <ThemeContext.Provider value={{ theme, toggle_theme }}>
