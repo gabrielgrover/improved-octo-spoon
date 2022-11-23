a47bc5964dbae0af8f28230d9b6e03e2d19c1e2b
Gabriel <12038578+gabrielgrover@users.noreply.github.com>
2022-11-23 14:35:51 -0800

    commenting-redux update style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,15 +2,16 @@ import { Blog } from "../../Blog";
 import { load_blog_html } from "../../apis/blog_api";
 import { PageHeading } from "../components";
 import { Constants } from "../../utils";
+import styles from "./about.module.css";
 
 const About = async () => {
   const blog_html = await load_blog_html(String(Constants.ABOUT_ME_BLOG_ID));
 
   return (
-    <>
+    <div className={styles.container}>
       <PageHeading>Void log 0</PageHeading>
       <Blog html={blog_html} />
-    </>
+    </div>
   );
 };
 
