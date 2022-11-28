bca0f1c5650d955234f07c98ef4d4d39edb29ab3
Gabriel <12038578+gabrielgrover@users.noreply.github.com>
2022-11-27 23:14:35 -0800

    fix_token_cache_issue token test

--- app/components/CommentSection/CommentSection.tsx
+++ app/components/CommentSection/CommentSection.tsx
@@ -1,11 +1,24 @@
 import { CommentInput } from "../CommentInput/CommentInput";
 import styles from "./comment_section_styles.module.css";
+import { generate_api_token } from "../../../utils/api_token";
+import * as F from "fp-ts/function";
+import * as E from "fp-ts/Either";
 
 type Props = {
   blog_id: string;
 };
 
 export const CommentSection: React.FC<Props> = (props) => {
+  const api_token = F.pipe(
+    generate_api_token(),
+    E.fold(
+      (e) => e.message,
+      (i) => i
+    )
+  );
+
+  console.log({ api_token });
+
   return (
     <div className={styles.comment_input_container}>
       <h2 className={styles.comment_heading}>Penny for your thoughts</h2>
